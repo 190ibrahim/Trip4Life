2db28ab72a2c68cf9526021a30513b94cea3effc
190ibrahim <99482003+190ibrahim@users.noreply.github.com>
2023-01-23 02:41:08 +0100

    Update dbcon.php

--- includes/dbcon.php
+++ includes/dbcon.php
@@ -1,9 +1,9 @@
 <?php
 
-$db = 'boat';
-$host  = 'localhost';
-$dbusername = 'root';
-$password = '';
+$db = 'heroku_136790215a90f3a';
+$host  = 'eu-cdbr-west-03.cleardb.net';
+$dbusername = 'b43bb080256327';
+$password = '9b95ee60';
 
 try {
     $pdo = new PDO("mysql:host=$host; dbname=$db;", $dbusername, $password);
